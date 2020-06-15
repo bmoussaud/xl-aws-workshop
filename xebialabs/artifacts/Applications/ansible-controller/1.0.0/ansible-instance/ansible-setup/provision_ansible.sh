51b9d8ec4b980161633bd40750bdefd01d0c3fad
Benoit Moussaud <bmoussaud@xebialabs.com>
2020-06-15 11:22:27 +0000

    Update provision_ansible.sh

--- xebialabs/artifacts/Applications/ansible-controller/1.0.0/ansible-instance/ansible-setup/provision_ansible.sh
+++
@@ -1,6 +0,0 @@
-set -x
-apt-get update --yes
-apt-get install --yes software-properties-common
-apt-add-repository --yes --update ppa:ansible/ansible
-apt-get install --yes ansible
-ansible-playbook --version
