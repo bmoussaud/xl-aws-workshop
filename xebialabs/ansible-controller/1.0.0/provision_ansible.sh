cdbfa6169180d34b6912d3f973fad9b3dcad81a6
Benoit Moussaud <bmoussaud@xebialabs.com>
2020-06-15 09:23:31 +0200

    fix provision_ansible.sh with sudo prefix

--- xebialabs/ansible-controller/1.0.0/provision_ansible.sh
+++ xebialabs/ansible-controller/1.0.0/provision_ansible.sh
@@ -1,6 +1,6 @@
-apt update
-apt install --yes software-properties-common
-apt-add-repository --yes --update ppa:ansible/ansible
-apt install --yes ansible
+sudo apt update
+sudo apt install --yes software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install --yes ansible
 ansible-playbook --version
 
